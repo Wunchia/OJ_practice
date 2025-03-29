5fcf24e09104806382ead9a205da095c379ed7f6
Yamada Bunnka <157354488+wunchia@users.noreply.github.com>
2025-03-29 22:48:44 +0800

    update

--- DS_algorithm/chapter2/2.1Stack/main.cpp
+++ DS_algorithm/chapter2/2.1Stack/main.cpp
@@ -1,25 +1,81 @@
-#include <iostream>
+#include <stdio.h>
+#define MaxSize 20
+typedef int ElemType;
 
-// TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-int main() {
-    // TIP Press <shortcut actionId="RenameElement"/> when your caret is at the
-    // <b>lang</b> variable name to see how CLion can help you rename it.
-    auto lang = "C++";
-    std::cout << "Hello and welcome to " << lang << "!\n";
+typedef struct{
+    ElemType data[MaxSize];
+    int top;
+}SqStack;
 
-    for (int i = 1; i <= 5; i++) {
-        // TIP Press <shortcut actionId="Debug"/> to start debugging your code.
-        // We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/>
-        // breakpoint for you, but you can always add more by pressing
-        // <shortcut actionId="ToggleLineBreakpoint"/>.
-        std::cout << "i = " << i << std::endl;
+void InitStack(SqStack &S){
+    S.top=-1;//栈为空
+}
+
+bool IsEmpty(SqStack S){
+    if(S.top==-1){
+        return true;
+    } else{
+        return false;
+    }
+}
+
+bool Push(SqStack &S,ElemType i){
+    if(MaxSize-1==S.top){
+        return false;
+    }
+    S.data[++S.top]=i;
+    return true;
+}
+
+bool GetTop(SqStack S,ElemType &m){
+    if(IsEmpty(S)){
+        return false;
+    }
+    m=S.data[S.top];
+    return true;
+}
+
+bool Pop(SqStack &S,ElemType &m){
+    if(IsEmpty(S)){
+        return false;
+    }
+    m=S.data[S.top--];
+    return true;
+}
+
+int main(){
+    SqStack S;
+    //初始化
+    InitStack(S);
+    //判空
+    bool flag=IsEmpty(S);
+    if(flag){
+        printf("stack is empty\n");
+    }else{
+        printf("stack is not empty\n");
+    }
+
+    //入栈
+    for(int i=3;i>0;i--){
+        Push(S,i);
+    }
+
+    //取栈顶元素
+    ElemType m;
+    flag=GetTop(S,m);
+    if(flag){
+        printf("get %d\n",m);
+    }else{
+        printf("get nothing\n");
+    }
+
+    //出栈
+    flag=Pop(S,m);
+    if(flag){
+        printf("pop %d, top=%d\n",m,S.top);
+    }else{
+        printf("something went wrong\n");
     }
 
     return 0;
 }
-
-// TIP See CLion help at <a
-// href="https://www.jetbrains.com/help/clion/">jetbrains.com/help/clion/</a>.
-//  Also, you can try interactive lessons for CLion by selecting
-//  'Help | Learn IDE Features' from the main menu.
